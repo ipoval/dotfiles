8316e384fc94d38f0f9573c5767ad825b371a797
Ivan Povalyukhin <ivanpoval@gmail.com>
2013-08-31 19:20:34 -0700

    add to chef recipe pstree, psgrep and pidof

--- chef/cookbooks/ubuntu_toolbox/recipes/default.rb
+++ chef/cookbooks/ubuntu_toolbox/recipes/default.rb
@@ -1,11 +1,13 @@
+should_brew = ->(brew_library) { File.exists? File.join(node[:homebrew_dir], brew_library) }
+
 execute 'brew install node and npm' do
   command 'brew install node'
-  not_if { File.exists? File.join(node[:homebrew_dir], 'node/') }
+  not_if { should_brew.call 'node' }
 end
 
 execute 'brew install imagemagick' do
   command 'brew install imagemagick'
-  not_if { File.exists? File.join(node[:homebrew_dir], 'imagemagick/') }
+  not_if { should_brew.call 'imagemagick' }
 end
 
 # execute 'brew install libyaml' do
@@ -15,30 +17,45 @@ end
 
 execute 'brew install htop' do
   command 'brew install htop'
-  not_if { File.exists? File.join(node[:homebrew_dir], 'htop/') }
+  not_if { should_brew.call 'htop' }
 end
 
 execute 'brew install tree' do
   command 'brew install tree'
-  not_if { File.exists? File.join(node[:homebrew_dir], 'tree/') }
+  not_if { should_brew.call 'tree' }
 end
 
 execute 'brew install ssh-copy-id' do
   command 'brew install ssh-copy-id'
-  not_if { File.exists? File.join(node[:homebrew_dir], 'ssh-copy-id/') }
+  not_if { should_brew.call 'ssh-copy-id' }
 end
 
 execute 'brew install proctools' do
   command 'brew install proctools'
-  not_if { File.exists? File.join(node[:homebrew_dir], 'proctools/') }
+  not_if { should_brew.call 'proctools' }
 end
 
 execute 'brew install graphviz' do
   command 'brew install graphviz'
-  not_if { File.exists? File.join(node[:homebrew_dir], 'graphviz/') }
+  not_if { should_brew.call 'graphviz' }
 end
 
 execute 'brew install nmap' do
   command 'brew install nmap'
-  not_if { File.exists? File.join(node[:homebrew_dir], 'nmap/') }
+  not_if { should_brew.call 'nmap' }
+end
+
+execute 'brew install psgrep' do
+  command 'brew install psgrep'
+  not_if { should_brew.call 'psgrep' }
+  notifies :run, 'execute[pidof]', :immediately
+  notifies :run, 'execute[pstree]', :immediately
+end
+execute 'pidof' do
+  command 'brew install pidof'
+  action :nothing
+end
+execute 'pstree' do
+  command 'brew install pstree'
+  action :nothing
 end
