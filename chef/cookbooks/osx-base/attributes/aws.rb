f4ca2f278b70dbb1cc7ec4ca3cb2cb931ebc300f
Ivan Povalyukhin <ivanpoval@gmail.com>
2013-09-07 16:03:46 -0700

    update .ssh config template

---
+++ chef/cookbooks/osx-base/attributes/aws.rb
@@ -0,0 +1,3 @@
+default[:ssh_config][:ec2_micro][:host]      = 'ipovalawsmicro'
+default[:ssh_config][:ec2_micro][:host_name] = 'ec2-23-23-6-244.compute-1.amazonaws.com'
+default[:ssh_config][:ec2_micro][:user]      = 'ubuntu'
--- chef/cookbooks/osx-base/recipes/config_ssh.rb
+++ chef/cookbooks/osx-base/recipes/config_ssh.rb
@@ -1,4 +1,6 @@
 template "#{node[:homedir]}/.ssh/config" do
   source 'config_ssh.erb'
   owner node[:user]
+
+  variables(identity_file: '~/.ssh/ipovalec2-pkey.pem')
 end
