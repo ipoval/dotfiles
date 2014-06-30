2b6829db2164f1dfa3abc5317e04cfd0c1a3f112
Ivan Povalyukhin <ivanpoval@gmail.com>
2014-06-30 00:00:55 -0700

    added .git_template to keep global git hooks
    added global git hook

--- chef/cookbooks/osx-base/recipes/config_git.rb
+++ chef/cookbooks/osx-base/recipes/config_git.rb
@@ -1,3 +1,16 @@
+directory "#{node[:homedir]}/.git_template/hooks" do
+  owner node[:user]
+  recursive true
+  action :create
+  only_if { !File.exists?("#{node[:homedir]}/.git_template/hooks") }
+end
+
+template "#{node[:homedir]}/.git_template/hooks/pre-commit" do
+  source 'git_hook_precommit.erb'
+  owner node[:user]
+  mode 0777
+end
+
 execute '.gitconfig' do
   command 'git config --global user.name ipoval'
   command 'git config --global user.email ivanpoval@gmail.com'
@@ -7,6 +20,7 @@ execute '.gitconfig' do
   command 'git config --global color.branch auto'
   command 'git config --global alias.co checkout'
   command 'git config --global help.autocorrect 1'
+  command "git config --global init.templatedir '#{node[:homedir]}/.git_template/'"
 end
 
 template "#{node[:homedir]}/.gitignore" do
