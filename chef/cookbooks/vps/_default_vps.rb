39ec3061888cfcc95ea551127188a7c4c2233865
Ivan Povalyukhin <ivanpoval@gmail.com>
2014-06-30 00:06:14 -0700

    fixing double string code style

--- chef/cookbooks/vps/_default_vps.rb
+++ chef/cookbooks/vps/_default_vps.rb
@@ -2,11 +2,11 @@
 
 package 'git-core' # apt-get install git-core | sudo port install git-core
 
-include_recipe "nginx::source"
+include_recipe 'nginx::source'
 
 user node[:user][:name] do
   password node[:user][:password]
-  gid "admin"
+  gid 'admin'
   home "/home/#{node[:user][:name]}"
   supports manage_home: true
 end
@@ -21,11 +21,11 @@ end
 
 file "/home/#{node[:user][:name]}/example/index.html" do
   owner node[:user][:name]
-  content "<h1>Hello World</h1>"
+  content '<h1>Hello World</h1>'
 end
 
 file "#{node[:nginx][:dir]}/sites-available/example" do
   content "server { root /home/#{node[:user][:name]}/example; }"
 end
 
-nginx_site "example"
+nginx_site 'example'
